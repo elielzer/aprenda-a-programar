527b15e786cc51f4767c2f03e5012737cb260596
Elielzer Nuayed <enuayed@gmail.com>
2022-08-23 23:50:45 -0300

    Cad turismo

---
+++ tarefa-dia-2/pratica-1.js
@@ -0,0 +1,7 @@
+// Declarando variáveis para mensagem de turismo
+
+let meuNome //Nome 
+let meuEmail //email
+let minhaProfissão //Profissão
+let minhaIdade // Idade
+let minhaPreferencia //o lugar que você sonha um dia conhecer (pode ser um país, cidade ou um local mesmo)
