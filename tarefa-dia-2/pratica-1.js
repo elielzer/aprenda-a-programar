f07b277e9678837b635bb4fe0a6b75a8d1eb67f5
Elielzer Nuayed <enuayed@gmail.com>
2022-08-24 12:07:23 -0300

    Envio mala direta

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -2,7 +2,7 @@
 
 let meuNome //Nome 
 let meuEmail //email
-let minhaProfissão //Profissão
+let minhaProfissao //Profissão
 let minhaIdade // Idade
 let minhaPreferencia //o lugar que você sonha um dia conhecer (pode ser um país, cidade ou um local mesmo)
 
@@ -10,15 +10,22 @@ let minhaPreferencia //o lugar que você sonha um dia conhecer (pode ser um paí
 
 meuNome = "Elon Musk"
 meuEmail =  "tesla@gmail.com"
-minhaProfissão = "CEO"
+minhaProfissao = "CEO"
 minhaIdade = 30 
 minhaPreferencia = "Califórnia"
 
-// Processamento para saída de dados
+// Processamento para saída de dados check list
 
 console.log("------Dados do cliente de turismo--------")
 console.log("Nome-> " + meuNome)
 console.log("Idade-> " + minhaIdade + " anos")
 console.log("email-> " + meuEmail)
-console.log("Profissão-> " + minhaProfissão)
+console.log("Profissão-> " + minhaProfissao)
 console.log("Lugar de preferência para conhecer-> " + minhaPreferencia)
+
+// Processamento para mala direta
+
+console.log(`Olá ${meuNome}! Somos aqui da agência de turismo Viagem dos Sonhos.
+Estamos te escrevendo este email, pois acabamos de confirmar as compras das passagens aéreas para ${minhaPreferencia}.
+Confirma pra gente alguns dados? A sua idade é ${minhaIdade}, você é ${minhaProfissao} e o seu email é ${meuEmail}? Ficamos no aguardo.
+Muito obrigado e boa viagem! :D`)
