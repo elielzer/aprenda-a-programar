fbc603c9ef100ec236d4c01fbcb0dcbacca4f373
Elielzer Nuayed <enuayed@gmail.com>
2022-08-24 00:00:55 -0300

    Formulário preenchido

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -5,3 +5,11 @@ let meuEmail //email
 let minhaProfissão //Profissão
 let minhaIdade // Idade
 let minhaPreferencia //o lugar que você sonha um dia conhecer (pode ser um país, cidade ou um local mesmo)
+
+// Atribuições
+
+meuNome = "Elon Musk"
+meuEmail =  "tesla@gmail.com"
+minhaProfissão = "CEO"
+minhaIdade = 30 
+minhaPreferencia = "Califórnia"
