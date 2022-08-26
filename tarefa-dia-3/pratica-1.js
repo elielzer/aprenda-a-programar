b7e14c974c6dce56289287c7406a23ba7e37d063
Elielzer Nuayed <enuayed@gmail.com>
2022-08-26 13:40:31 -0300

    Comandos de repetição

---
+++ tarefa-dia-3/pratica-1.js
@@ -0,0 +1,21 @@
+// Testando o comando de repetição no JS
+// a saída do processamento irá apresentar numeros de 1 até "n"
+// "n" será uma possível entrada de usuário (um número inteiro)
+
+// primeiro uma possível entrada de usuário é esperada
+n = 9
+
+let i = 1 // "i" é o contador funcional do processo de repetição
+          // o valor em "i" é que será apresentado 
+
+while ( i <= n ) {
+
+    console.log (i) // o que resultar nessa linha será repetido
+                    // para refletir o que se pede.
+
+    i +=1           // aqui o contador será incrementado de uma 
+                    // unidade para alcançar o próximo inteiro a ser apresentado.
+    
+} // Este é o comando de repetição
+
+// Fim
